5ac23fd6aaeb3f49f1e9d05d4d9966049951acc0
Juan Osorio <juan.michael.osorio@gmail.com>
2020-05-09 22:02:59 -0500

    updated MovieAPI.md with better info and added $.get function to Script.js

--- assets/script.js
+++ assets/script.js
@@ -50,13 +50,23 @@ $(document).ready(function () {
                 $.ajax({ url: queryURL, method: 'GET' })
                 var tempFahrenheit = Math.round(((response.main.temp) - 273.15) * 1.80 + 32);
                 console.log(response);
-                
+
                 var main = response.weather.temp;
 
 
             });
         }
-
-
     });
+
+    //retrieving movies by genre code
+    // see MovieAPI.md for more genre codes
+    var genreCode = 28 
+    var movieURL = 'https://api.themoviedb.org/3/discover/movie?api_key=e7f668e97c13dfe1d5f7100b7a29d6bd&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=' + genreCode
+    $.get(movieURL, function (response) {
+        //examples of key retrieval paths
+        console.log(response.page);
+        console.log(response.results[3]);
+        console.log(response.results[3].title);
+    });
+    
 });
